b6e790aada1cbf44c2109a952be269f33bbf32c1
swarnadwipnath1 <swarnadwip@excellisit.net>
2022-11-01 18:03:15 +0530

    login and register for user and business owner

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+    /** 
+     * login api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
+            'password' => 'required|min:8',
+            'user_type' => 'required|in:USER,BUSINESS_OWNER',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors(), 'status' => false], 401);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::where('email', $request->email)->select('id', 'name', 'email')->first();
+            if ($request->user_type == 'USER') {
+                if ($user->hasRole('USER')) {
+                    $data['token'] =  $user->createToken('Groomly')->accessToken;
+                    $data['user'] = $user->makeHidden('roles');
+                    return response()->json(['data' => $data, 'status' => true, 'message' => 'Logged in successfully.'], $this->successStatus);
+                } else {
+                    return response()->json(['messager' => 'Email id & password was invalid!', 'status' => false], 401);
+                }
+            } else {
+                if ($user->hasRole('BUSINESS_OWNER')) {
+                    $data['token'] =  $user->createToken('Groomly')->accessToken;
+                    $data['user'] = $user->makeHidden('roles');
+                    return response()->json(['data' => $data, 'status' => true, 'message' => 'Logged in successfully.'], $this->successStatus);
+                } else {
+                    return response()->json(['messager' => 'Email id & password was invalid!', 'status' => false], 401);
+                }
+            }
+        } else {
+            return response()->json(['messager' => 'Email id & password was invalid!', 'status' => false], 401);
+        }
+    }
+
+    /** 
+     * Register api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'email'    => 'required|email|unique:users|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
+            'password' => 'required|min:8',
+            'user_type' => 'required|in:USER,BUSINESS_OWNER',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors(), 'status' => false], 401);
+        }
+        $input = $request->all();
+        $user = new User;
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+        $user->password = bcrypt($input['password']);
+        $user->save();
+        if ($request->user_type == 'USER') {
+        } else {
+            $user->assignRole('BUSINESS_OWNER');
+        }
+
+        $data =  $user->makeHidden('roles', 'updated_at', 'created_at');
+        return response()->json(['data' => $data, 'status' => true, 'message' => 'Registered sucessfully'], $this->successStatus);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//User & business owner portal authentication
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 });
