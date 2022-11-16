14de81551b272ab5a11b33c1f4a35b1c387cbc3f
swarnadwipnath1 <swarnadwip@excellisit.net>
2022-11-16 19:25:49 +0530

    small changes

--- app/Http/Controllers/Frontend/BookNowController.php
+++ app/Http/Controllers/Frontend/BookNowController.php
@@ -17,37 +17,36 @@ class BookNowController extends Controller
     public function bookNow($id)
     {
         if (!Auth::check()) {
-            return redirect()->route('login')->with('error','Please login first to booked the appointment!!');
-        }else{
+            return redirect()->route('login')->with('error', 'Please login first to booked the appointment!!');
+        } else {
             if (Auth::user()->hasRole('USER')) {
                 $id = base64_decode($id);
                 $service = Service::findOrFail($id);
                 $times = BookingTime::get();
-                return view('frontend.book-now')->with(compact('service','times'));
+                return view('frontend.book-now')->with(compact('service', 'times'));
             } else {
-                return redirect()->route('login')->with('error','Please login as a user!!');
+                return redirect()->route('login')->with('error', 'Please login as a user!!');
             }
         }
-        
     }
 
     public function submitAppointment(Request $request)
     {
         if (!Auth::check()) {
-            return redirect()->route('login')->with('error','Please login first to booked the appointment!!');
+            return redirect()->route('login')->with('error', 'Please login first to booked the appointment!!');
         } else {
             if (Auth::user()->hasRole('USER')) {
                 $request->validate([
-                    'booking_date'=>'required',
-                    'booking_time_id'=>'required',
+                    'booking_date' => 'required',
+                    'booking_time_id' => 'required',
                     'name' => 'required',
-                    'email'=> 'required',
+                    'email' => 'required',
                     'phone' => 'required|numeric'
-                ],[
+                ], [
                     'booking_time_id.required' => 'Booking time filed is required.'
                 ]);
-                 $count = Appointment::where(['service_id'=>$request->service_id, 'booking_date'=>$request->booking_date, 'booking_time_id'=> $request->booking_time_id])->count();
-                if ($count>=25) {
+                $count = Appointment::where(['service_id' => $request->service_id, 'booking_date' => $request->booking_date, 'booking_time_id' => $request->booking_time_id])->count();
+                if ($count >= 25) {
                     return redirect()->back()->with('error', 'Slot not available!!');
                 }
                 $appointment = new Appointment;
@@ -67,7 +66,7 @@ class BookNowController extends Controller
                 Mail::to($email)->send(new BookAppointmentMail($maildata));
                 return redirect()->route('appointments.index')->with('message', 'Thank you for booking slot. Our team will be in touch with you soon');
             } else {
-                return redirect()->route('login')->with('error','Please login as a user!!');
+                return redirect()->route('login')->with('error', 'Please login as a user!!');
             }
         }
     }
